321733bc05ba04e66bba09e93757f61fb62aee35
Pablo Spaltro <pablospaltro5@gmail.com>
2023-09-26 22:30:51 -0300

    Refactorizo métodos de juego

--- egg-excepciones-ej-5/src/entidades/Juego.java
+++ egg-excepciones-ej-5/src/entidades/Juego.java
@@ -1,61 +1,67 @@
 package entidades;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Juego {
 
-    Scanner scanner = new Scanner(System.in);
-    Random random = new Random();
+    private final Scanner scanner = new Scanner(System.in);
+    private final Random random = new Random();
 
-    public void juegoAdivinarNumero(){
-        boolean volverAJugar = true;
+    public void juegoAdivinarNumero() {
         System.out.println("A D I V I N A D O R");
-        System.out.println("Adivine el numero entre 1 y 500!");
+        System.out.println("Adivine el número entre 1 y 500!");
 
+        boolean volverAJugar;
         do {
-            int numeroAleatorio = random.nextInt(500) + 1;
-            int contadorIntentos = 0;
-            boolean entradaValida = true;
-
-            try {
-                while (true) {
-                    System.out.println("\nIngrese un numero: ");
-                    int numeroIngresado = scanner.nextInt();
-                    scanner.nextLine();
-
-                    if (numeroIngresado == numeroAleatorio) {
-                        System.out.println("\n-- ¡Adivinaste! --");
-                        System.out.println("El numero buscado era "+numeroAleatorio);
-                        System.out.println("Cantidad de intentos realizados: " + contadorIntentos);
-                        break;
-                    } else if (numeroIngresado < numeroAleatorio) {
-                        System.out.println("\n-- El numero que tenes que adivinar es MAYOR al ingresado --");
-                    } else {
-                        System.out.println("\n-- El numero que tenes que adivinar es MENOR al ingresado --");
-                    }
-                    contadorIntentos++;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("\n(Ups! Valor ingresado no válido, asegurate de ingresar un número)");
-                entradaValida = false;
-                scanner.nextLine();
-            }
-
-            if (!entradaValida) {
-                continue;
-            }
+            jugarUnaPartida();
 
             System.out.println("\nJugar otra vez? S / N");
             String opcion = scanner.nextLine();
-            if (opcion.equalsIgnoreCase("N")) {
-                volverAJugar = false;
-            }
+            volverAJugar = opcion.equalsIgnoreCase("S");
         } while (volverAJugar);
     }
 
+    private void jugarUnaPartida() {
+        int numeroAleatorio = random.nextInt(500) + 1;
+        int contadorIntentos = 0;
+
+        while (true) {
+            System.out.println("\nIngrese un número: ");
+            int numeroIngresado = obtenerNumeroIngresado();
+
+            if (numeroIngresado == numeroAleatorio) {
+                mostrarResultadoExitoso(numeroAleatorio, contadorIntentos);
+                break;
+            } else if (numeroIngresado < numeroAleatorio) {
+                System.out.println("\n-- El número que tienes que adivinar es MAYOR al ingresado --");
+            } else {
+                System.out.println("\n-- El número que tienes que adivinar es MENOR al ingresado --");
+            }
+
+            contadorIntentos++;
+        }
+    }
+
+    private int obtenerNumeroIngresado() {
+        while (true) {
+            try {
+                int numeroIngresado = scanner.nextInt();
+                scanner.nextLine();
+                return numeroIngresado;
+            } catch (InputMismatchException e) {
+                System.out.println("\n(Ups! Valor ingresado no válido, asegúrate de ingresar un número)");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    private void mostrarResultadoExitoso(int numeroAleatorio, int contadorIntentos) {
+        System.out.println("\n-- ¡Adivinaste! --");
+        System.out.println("El número buscado era " + numeroAleatorio);
+        System.out.println("Cantidad de intentos realizados: " + contadorIntentos);
+    }
 }
+
 
