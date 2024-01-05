6a43222614a93db63c5697b47b69d2f68f9ff823
Pablo Spaltro <pablospaltro5@gmail.com>
2024-01-05 11:05:49 -0300

    Agrego test con DataProvider

--- egg-testng/src/test/java/org/example/AppTest.java
+++ egg-testng/src/test/java/org/example/AppTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.log4testng.Logger;
@@ -15,6 +16,15 @@ public class AppTest {
 
         Assert.assertEquals(a, "Hello");
         Assert.assertEquals(b, "Globant");
+    }
 
+    @DataProvider(name = "translateHello")
+    public static Object[][] inputData(){
+        return new Object[][] {{"Hello"}, {"Hola"}, {"Ciao"}};
+    }
+
+    @Test(dataProvider = "translateHello")
+    public void testTranslate(String data){
+        log.info(data);
     }
 }
